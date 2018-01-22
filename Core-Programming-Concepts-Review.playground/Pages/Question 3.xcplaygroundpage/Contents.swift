2d989f6b6fb1106d62b64b0fa675044015168694
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2018-01-22 13:49:39 -0500

    corrected question 3

--- Core-Programming-Concepts-Review.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Review.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
@@ -15,11 +15,11 @@
  */
 // Answer question 3 below
 var piggyBank : Double = 0.0
-piggyBank + 10
-piggyBank + 20
-piggyBank / 2
-piggyBank * 3
-piggyBank - 3
+piggyBank += 10
+piggyBank += 20
+piggyBank /= 2
+piggyBank *= 3
+piggyBank -= 3
 /*:
  ## Now share your understanding
  
